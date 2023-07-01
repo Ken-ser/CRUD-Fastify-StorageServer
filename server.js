3f08426c5a26aef074981fae7024212a309c402e
Ken-ser <karimbachir2001@gmail.com>
2023-07-01 22:52:09 +0200

    Project initialized. Server listen and test route implemented.

---
+++ server.js
@@ -0,0 +1,23 @@
+import Fastify from "fastify";
+
+const fastify = Fastify();
+
+//test route
+fastify.route({
+    method: "GET",
+    path:"/test",
+    handler: async (request, reply) => {
+        return {
+            status: "OK"
+        };
+    }
+});
+
+// Run the server
+try {
+    await fastify.listen({ port: 3000 });
+    console.log("- Server started");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
