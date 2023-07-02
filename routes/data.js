9c68f730fb3a550ad1f39408dc3dc94078bad7c5
Ken-ser <karimbachir2001@gmail.com>
2023-07-02 12:19:40 +0200

    Started "data" route developing - Created data.json: keeps key-data bindings - Working "GET /data/:key" method (prototype)

--- routes/data.js
+++ routes/data.js
@@ -1,14 +1,33 @@
 import FP from "fastify-plugin"
+import FS from "fs/promises"
 
-async function data (fastify, opts){
+async function data(fastify, opts) {
     //test route
     fastify.route({
         method: "GET",
-        path: "/data",
+        path: "/data/:key",
         handler: async (request, reply) => {
-            return {
-                status: "Data routes"
-            };
+            try {
+                //read file
+                const rawData = await FS.readFile("./db/data.json");
+                //JSON parse file
+                const jsonData = JSON.parse(rawData);
+                //search key
+                for (const o of jsonData) {
+                    if (o.key == request.params.key) {
+                        //key found
+                        console.log("- Key " + o.key + " found with data: " + o.data);
+                        return {
+                            key: o.key,
+                            data: o.data
+                        };
+                    }
+                }
+                return { status: "Key not found"}
+            } catch (error) {
+                console.error(`Got an error trying to read the file: ${error.message}`);
+                return { status: "error"}
+            }
         }
     });
 };
