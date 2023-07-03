ea9544307d758e25a6fee60616ca2f823a237fef
Ken-ser <karimbachir2001@gmail.com>
2023-07-03 19:12:57 +0200

    Working "PATCH /data" method (prototype) - Changes "data" binded to specified "key"

--- routes/data.js
+++ routes/data.js
@@ -11,7 +11,7 @@ async function data(fastify, opts) {
             const rawData = await FS.readFile("./db/data.json");
             //JSON parse file
             const jsonData = JSON.parse(rawData);
-            jsonData
+
             //search key
             for (const o of jsonData) {
                 if (o.key == request.params.key) {
@@ -32,18 +32,47 @@ async function data(fastify, opts) {
         method: "POST",
         path: "/data",
         handler: async (request, reply) => {
-            //already parsed by fastify
+            //get body already parsed by fastify
             const bodyData = request.body;
-            //read file
+            //read and JSON parse file
             const dbData = JSON.parse(await FS.readFile("./db/data.json"));
             //add data at position 0
-            dbData.splice(0,0, bodyData);
+            dbData.splice(0, 0, bodyData);
 
             //write entire file
-            await FS.writeFile("./db/data.json", JSON.stringify(dbData, null, 2));
+            await FS.writeFile("./db/data.json", JSON.stringify(dbData, null, 4));
 
             reply.code(201);
-            return { info: "data stored" }; 
+            return { info: "data stored" };
+        }
+    });
+
+    //patch data by key
+    fastify.route({
+        method: "PATCH",
+        path: "/data",
+        handler: async (request, reply) => {
+            //get body already parsed by fastify
+            const bodyData = request.body;
+            //read and JSON-parse file
+            const dbData = JSON.parse(await FS.readFile("./db/data.json"));
+
+            //search key
+            for (const object of dbData) {
+                if (object.key == bodyData.key) {
+                    //key found
+
+                    //change data
+                    object.data = bodyData.data;
+
+                    //write entire file
+                    await FS.writeFile("./db/data.json", JSON.stringify(dbData, null, 4));
+
+                    return { info: "data patched" };
+                }
+            }
+            reply.code(404);
+            return { info: "Key not found" };
         }
     });
 };
