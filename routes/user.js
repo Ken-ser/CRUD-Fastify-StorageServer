ecb16256675de28de30abb1955688a03ed18ddd2
Ken-ser <karimbachir2001@gmail.com>
2023-07-04 15:39:46 +0200

    Working "POST /login" method (prototype)

--- routes/user.js
+++ routes/user.js
@@ -29,6 +29,29 @@ async function user(fastify, opts) {
             return { info: "Signed up" };
         }
     });
+
+    //sign in user
+    fastify.route({
+        method: "POST",
+        path: "/login",
+        handler: async (request, reply) => {
+            const email = request.body.email;
+            const password = request.body.password;
+
+            //read and JSON.parse file
+            const jsonUsers = JSON.parse(await FS.readFile("./db/users.json"));
+
+            //check user
+            for (const user of jsonUsers) {
+                if (user.email == email && user.password == password) {
+                    return { info: "Signed in" };
+                }
+            }
+
+            reply.code(401);
+            return { info: "Wrong email or password" };
+        }
+    });
 };
 
 export default FP(user);
